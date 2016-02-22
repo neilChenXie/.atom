da2c272244cc950e1437db0d8229265741c5dc8d
Chen Xie <xcmickey@gmail.com>
2016-02-22 20:03:37 +0800

    update

--- plugin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apm install vim-mode
-apm install vim-mode-visual-block
-apm install vim-surround
-apm install git-plus
-apm install project-manager
-apm install term2
-apm install script
